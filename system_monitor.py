475cc9627b8dafdb9544a91f8e5f58eadba0cfdc
David Carmichael (windows) <carmichaelits@gmail.com>
2024-08-26 01:56:26 +0100

    add logo

--- system_monitor.py
+++ system_monitor.py
@@ -1,103 +1,37 @@
-#
-# SYSTEM MONITOR
-#
+import tkinter as tk
 import pathlib
-import subprocess
-import time
-import tomllib
 
-import psutil
-import requests
+def walk_find_logo():
+    cwd = pathlib.Path(__file__).parent
+    print(cwd)
 
-PARENT_DIR = pathlib.Path(__file__).parent
-TOML_FILE = PARENT_DIR / 'config.toml'
+    logo = cwd / "logo.gif"
 
-if not TOML_FILE.exists():
-    up = PARENT_DIR.parent
-    TOML_FILE = up / 'config.toml'
+    if logo.exists():
+        return logo
 
-CONFIG = tomllib.loads(TOML_FILE.read_text())
-UUID = None
+    for r in range(3):
+        cwd = cwd.parent
+        logo = cwd / "logo.gif"
+        if logo.exists():
+            return logo
 
+    raise FileNotFoundError("logo.png not found")
 
-def get_network_info() -> dict[str, dict[str, int | str | None]]:
-    _networks = psutil.net_if_addrs()
-    _network_counters = psutil.net_io_counters(pernic=True)
-    _return = {}
+class SystemMonitor(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("System Monitor")
+        self.geometry("400x200")
 
-    for network, info in _networks.items():
-        for i in info:
-            if i.family == 2:
-                _return[network] = {
-                    "ip": i.address,
-                    "netmask": i.netmask,
-                    "bytes_sent": _network_counters[network].bytes_sent,
-                    "bytes_recv": _network_counters[network].bytes_recv
-                }
+        self.logo = tk.PhotoImage(file=walk_find_logo())
+        self.image = tk.Label(self, image=self.logo)
+        self.image.pack()
 
-    return _return
+        self.button = tk.Button(self, text="Quit", command=self.quit)
+        self.button.pack()
 
 
-def get_processes() -> list[dict[str, str]]:
-    return [p.as_dict(attrs=['pid', 'name', 'username']) for p in psutil.process_iter()]
-
-
-def disk_usage() -> dict[str, float]:
-    return {
-        'total': psutil.disk_usage('/').total,
-        'used': psutil.disk_usage('/').used,
-        'free': psutil.disk_usage('/').free
-    }
-
-
-def get_cpu_usage() -> float:
-    return psutil.cpu_percent(interval=1)
-
-
-def get_memory_usage() -> float:
-    return psutil.virtual_memory().percent
-
-
-def get_windows_uuid() -> str:
-    if psutil.WINDOWS:
-        s = subprocess.check_output('wmic csproduct get uuid').strip()
-        s = s.decode('ascii').split("\n")
-        return s[1].strip()
-    return "Not Windows"
-
-
-def gather_info() -> None:
-    global UUID
-
-    _disk = disk_usage()
-    _processes = get_processes()
-    _network_info = get_network_info()
-
-    try:
-        requests.post(
-            CONFIG['url'],
-            json={
-                "system_id": CONFIG['system_id'],
-                "url": CONFIG['url'],
-                "interval": CONFIG['interval'],
-                "windows_uuid": UUID,
-                "epoch": int(time.time()),
-                "stats": {
-                    "cpu_usage": get_cpu_usage(),
-                    "memory_usage": get_memory_usage(),
-                    "disk_usage": _disk,
-                    "processes": _processes,
-                    "network_info": _network_info,
-                }
-            }
-        )
-    except requests.exceptions.ConnectionError:
-        pass
-
-
-if __name__ == '__main__':
-    UUID = get_windows_uuid()
-
-    while True:
-        time.sleep(CONFIG['interval'])
-        gather_info()
+if __name__ == "__main__":
+    app = SystemMonitor()
+    app.mainloop()
